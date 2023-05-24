86c57a9d1d96367e797e89b1dc7a04c93b3b6b1c
Yuri Shornikov <dbonlyfun@gmail.com>
2023-05-24 16:50:55 +0300

    finish

--- tests/demo/test_api.py
+++ tests/demo/test_api.py
@@ -2,15 +2,34 @@ import pytest
 from django.contrib.auth.models import User
 from rest_framework.test import APIClient
 
+from model_bakery import baker
+
 from demo.models import Message
+
+@pytest.fixture#фикстура для замены повторяемого кода
+def client():
+    return APIClient()
+
+@pytest.fixture
+def user():
+    return User.objects.create_user('admin')
+
+@pytest.fixture
+def message_factory():
+    def factory(*args, **kwargs):
+        return baker.make(Message, *args, **kwargs)
+
+    return factory
 
 
 @pytest.mark.django_db
-def test_api():
+def test_api(client, user, message_factory):
     #Arrange - то, что мы достаем из бд и помещаем
-    client = APIClient()
-    User.objects.create_user('admin')
-    Message.objects.create(user_id=1, text='text')
+    # client = APIClient()
+    # User.objects.create_user('admin')
+    messages = message_factory(_quantity=10)#создание 10 сообщений
+
+    # Message.objects.create(user_id=user.id, text='text')#user_id=1
 
     #Act - наши действия с параметрами
     response = client.get('/messages/')
@@ -18,7 +37,21 @@ def test_api():
     #Assert  - проверка действий
     assert response.status_code == 200
     data = response.json()
-    assert len(data) == 1
-    assert data[0]['text'] == 'text'
+    # assert len(data) == 1
+    assert len(data) == len(messages)
+    # assert data[0]['text'] == 'text'
+    for i, m in enumerate(data):
+        assert m['text'] == messages[i].text
 
-test_api()
+# test_api()
+
+@pytest.mark.django_db
+def test_create_message(client, user):#подставляем клиента и пользователя
+
+    count = Message.objects.count()
+    response = client.post('/messages/', data={'user': user.id, 'text': 'test text'})#, format='json'
+    assert response.status_code == 201
+    assert Message.objects.count() == count + 1#увеличение количества сообщени после каждого запроса
+
+
+
